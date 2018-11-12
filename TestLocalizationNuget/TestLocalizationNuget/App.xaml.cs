ad19a64ed02e222931cf68cc4ec45b829e1a44a1
magiciangambit <khairulariffin@gmx.com>
2018-11-12 10:55:20 +0800

    update repo

---
+++ TestLocalizationNuget/TestLocalizationNuget/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Plugin._121ResxLocalization;
+
+using Xamarin.Forms;
+
+namespace TestLocalizationNuget
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                Boolean checkLocal = Cross_121ResxLocalization.IsSupported;
+                I_121ResxLocalization iService = Cross_121ResxLocalization.Current;
+
+                
+                
+                //I_121ResxLocalization iService = DependencyService.Get<I_121ResxLocalization>();
+                // determine the correct, supported .NET culture
+                var ci = iService.GetCurrentCultureInfo();
+                Plugin._121ResxLocalization.TranslateExtension trE = new Plugin._121ResxLocalization.TranslateExtension(ci);
+                Plugin._121ResxLocalization.TranslateExtension.resourceBasename = "Plugin._121ResxLocalization.Resx.AppResources";
+                Plugin._121ResxLocalization.TranslateExtension.resourceAssembly = IntrospectionExtensions.GetTypeInfo(typeof(Plugin._121ResxLocalization.TranslateExtension)).Assembly;
+                Resx.AppResources.Culture = ci; // set the RESX for resource localization
+                iService.SetLocale(ci); // set the Thread for locale-aware methods
+            }
+
+            MainPage = new TestLocalizationNuget.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ TestLocalizationNuget/TestLocalizationNuget/TranslateExtension.cs
@@ -0,0 +1,68 @@
+﻿using Plugin._121ResxLocalization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestLocalizationNuget
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo ci = null;
+        //Type myType = typeof(Resx.AppResources);
+        //string namSpace = (string)myType.Namespace;
+
+        //const string ResourceId = "TestLocalizationNuget.Resx.AppResources";
+
+        //static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(ResourceId, IntrospectionExtensions.GetTypeInfo(typeof(TranslateExtension)).Assembly));
+
+        
+        //ResourceManager rscMgr = new ResourceManager(check, IntrospectionExtensions.GetTypeInfo(typeof(TranslateExtension)).Assembly);
+
+        public string Text { get; set; }
+
+        public static string GetCurrentNamespace()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().EntryPoint.DeclaringType.Namespace;
+        }
+
+        public TranslateExtension()
+        {
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                ci = DependencyService.Get<I_121ResxLocalization>().GetCurrentCultureInfo();
+            }
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var resourceTypeInfo = typeof(Label).GetTypeInfo();
+            var resourceBasename = resourceTypeInfo.FullName;
+            var resourceAssembly = resourceTypeInfo.Assembly;
+
+            var ResMgr = new ResourceManager(resourceBasename, resourceAssembly);
+
+            //var translation = resourceManager.GetString(Text, cultureInfo);
+            if (Text == null)
+                return string.Empty;
+
+            var translation = ResMgr.GetString(Text, ci);
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    string.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, resourceBasename, ci.Name),
+                    "Text");
+#else
+				translation = Text; // HACK: returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+    }
+}
