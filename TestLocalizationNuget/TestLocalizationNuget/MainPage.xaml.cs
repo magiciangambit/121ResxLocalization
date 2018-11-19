c168b56ff007b3fec41f0971149133699edd2f6b
magiciangambit <khairulariffin@gmx.com>
2018-11-19 14:48:53 +0800

    Update MainPage.xaml.cs

--- TestLocalizationNuget/TestLocalizationNuget/MainPage.xaml.cs
+++ TestLocalizationNuget/TestLocalizationNuget/MainPage.xaml.cs
@@ -13,5 +13,32 @@ namespace TestLocalizationNuget
 		{
 			InitializeComponent();
 		}
+		
+		//change language during runtime
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+		    if (((Button)sender).Text == "Set as English")
+		    {
+			setLanguage("en-US");
+		    }
+		    else if (((Button)sender).Text == "Set as Spanish")
+		    {
+			setLanguage("es-US");
+		    }
+		}
+
+		public void setLanguage(string languageCode)
+		{
+
+		    if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+		    {
+			CultureInfo ci = new CultureInfo(languageCode);
+			DependencyService.Get<Plugin._121ResxLocalization.I_121ResxLocalization>().SetLocale(ci); // set the Thread for locale-aware methods
+			Resx.AppResources.Culture = ci;
+
+			Application.Current.Properties["currentLanguage"] = languageCode;
+			Application.Current.MainPage = new MainPage();
+		    }
+		}
 	}
 }
